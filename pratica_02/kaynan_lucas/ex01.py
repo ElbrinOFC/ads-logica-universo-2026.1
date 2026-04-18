3b874fdd50121a83f16a86dc18078fdcb2d26330
Kaynanlucas <Kaynan360k@gmail.com>
2026-04-17 21:31:16 -0300

    entrega kaynan

--- pratica_02/kaynan_lucas/ex01.py
+++ pratica_02/kaynan_lucas/ex01.py
@@ -1,5 +1,4 @@
 nome = input("Qual o seu nome? ")
-idade = int(input("Qual o ano do seu nasciment1.651o? "))
-altura = float(input("Qual a sua altura? Atonio"))
-
-print("Olá, " + nome + "! Você tem " + str(2026 - idade) + " anos e mede " + str(altura) + " metros de altura.")kaynan
+idade = int(input("Qual o ano do seu nascimento? "))
+altura = float(input("Qual a sua altura? "))
+print("Olá, " + nome + "! Você tem " + str(2026 - idade) + " anos e mede " + str(altura) + " metros de altura.")
