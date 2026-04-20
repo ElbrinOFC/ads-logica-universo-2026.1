2b6e5d312c727d38fd12ed6577ec85048aeaa1de
Guilherme <guilhermelo.ads@gmail.com>
2026-04-19 21:57:10 -0300

    Revert "enviando-os-exercícios-da-prática02"
    This reverts commit fc277aa848c1bbfa292fa7487c79c69755dab9d7.

--- pratica_01/ex04.py
+++
@@ -1,8 +0,0 @@
-print("Exercício 04")
-
-idade = int(input("Digite sua idade: "))
-experiencia = int(input("Digite seus anos de experiência: "))
-
-acesso_liberado = idade >= 18 and experiencia >= 2
-
-print(f"Acesso Liberado: {acesso_liberado}")
