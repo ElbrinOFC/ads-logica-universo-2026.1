8d4d450ce8825bb88c7ce5936480b4c8e8099173
AllisonMRT <allisongorao2@gmail.com>
2026-05-08 12:03:03 -0300

    Delete pratica_01/allison-rodrigues/ex03.py

--- pratica_01/allison-rodrigues/ex03.py
+++
@@ -1,9 +0,0 @@
-fatias = int(input("Quantas fatias de pizza? "))
-pessoas = int(input("Quantas pessoas? "))
-
-fatias_por_pessoa = fatias // pessoas
-sobra = fatias % pessoas
-
-print(f"\n ---resultado---")
-print(f"Cada pessoa recebe {fatias_por_pessoa} fatias.")
-print(f"Sobram {sobra} fatias.")
