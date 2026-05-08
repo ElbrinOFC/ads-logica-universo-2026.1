71bbc54d92a3e9d5b2c05ec79fabd79dd4cfcf75
Luan Rodrigues <rodriguesluan854@gmail.com>
2026-05-08 00:30:34 -0300

    feat: entrega v1 - sistema de estoque

---
+++ projeto_integrador/squad_sem_nome/sistema_estoque/main.py
@@ -0,0 +1,32 @@
+from produtos import cadastrar_produto
+from movimentacao import realizar_venda
+from relatorios import curva_abc
+from utils import limpar_tela
+
+while True:
+    print("\n=== SISTEMA DE ESTOQUE ===")
+    print("1 - Cadastrar produto")
+    print("2 - Realizar venda")
+    print("3 - Relatório Curva ABC")
+    print("0 - Sair")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        limpar_tela()
+        cadastrar_produto()
+
+    elif opcao == "2":
+        limpar_tela()
+        realizar_venda()
+
+    elif opcao == "3":
+        limpar_tela()
+        curva_abc()
+
+    elif opcao == "0":
+        print("Encerrando sistema...")
+        break
+
+    else:
+        print("Opção inválida!")
