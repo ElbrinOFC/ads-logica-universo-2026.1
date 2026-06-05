ff04d2bea59fb42363c55f3047ed5a2cc691f8ec
Vitor Luis <vitorlgv@proton.me>
2026-06-05 11:18:58 -0300

    [PRÁTICA 05-1] Vitor Luis Gomes Vieira

--- pratica_05/vitor-luis/exec.py
+++
@@ -1,7 +0,0 @@
-for i in range(1, 26):
-    nome_arquivo = f"ex{i}.py"
-    
-    with open(nome_arquivo, "w", encoding="utf-8") as arquivo:
-        arquivo.write("")
-    
-    print(f"{nome_arquivo} criado.")
